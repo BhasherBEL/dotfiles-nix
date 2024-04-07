3fbf79c96dcab6f2c192bdb76eb37aa449b7898b
Brieuc Dubois <git@bhasher.com>
2024-04-07 04:21:10 +0200

    Media center configuration

--- flake.nix
+++ flake.nix
@@ -12,9 +12,11 @@
     hyprsome.url = "github:sopa0/hyprsome";
 
     nur.url = "github:nix-community/NUR";
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nur, nixos-hardware, ... }@inputs:
     let
       inherit (self) outputs;
       lib = nixpkgs.lib // home-manager.lib;
@@ -27,8 +29,11 @@
           specialArgs = { inherit inputs outputs; };
         };
         media-center = nixpkgs.lib.nixosSystem {
-          modules =
-            [ { nixpkgs.overlays = [ nur.overlay ]; } ./hosts/media-center ];
+          modules = [
+            { nixpkgs.overlays = [ nur.overlay ]; }
+            ./hosts/media-center
+            nixos-hardware.nixosModules.raspberry-pi-4
+          ];
           specialArgs = { inherit inputs outputs; };
         };
       };
--- hosts/media-center/default.nix
+++ hosts/media-center/default.nix
@@ -1,16 +1,83 @@
-{ ... }: {
-  imports = [
-    ./hardware-configuration.nix
-    ../shared/global
-    ../shared/users/kodi.nix
-  ];
+{ pkgs, ... }: {
+  imports =
+    [ ./hardware-configuration.nix ../shared/global ../shared/users/kodi.nix ];
 
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  networking = { hostName = "media-center"; };
+  networking = {
+    hostName = "media-center";
+    dhcpcd.enable = true;
+    interfaces.end0 = {
+      ipv4.addresses = [{
+        address = "10.0.0.10";
+        prefixLength = 23;
+      }];
+      wakeOnLan.enable = true;
+    };
+    defaultGateway = "10.0.0.1";
+    nameservers = [ "10.0.0.1" ];
+    firewall = {
+      allowedTCPPorts = [ 8080 ];
+      allowedUDPPorts = [ 8080 ];
+    };
 
-  services.openssh.enable = true;
+    wg-quick.interfaces.bxl-shp = {
+      address = [ "10.15.14.5/32" ];
+      privateKeyFile = "/etc/wireguard/bxl-shp.key";
+      dns = [ "10.15.14.1" ];
+      autostart = true;
+      peers = [{
+        publicKey = "Ft1qUCCs9GkpUfiotZU9Ueq1e9ncXr0PwWEyfLoc6Vs=";
+        presharedKeyFile = "/etc/wireguard/bxl-shp.psk";
+        allowedIPs = [ "0.0.0.0/0" ];
+        endpoint = "vpn.bhasher.com:51822";
+        persistentKeepalive = 25;
+      }];
+    };
+  };
+
+  hardware = {
+    raspberry-pi."4" = {
+      apply-overlays-dtmerge.enable = true;
+      fkms-3d.enable = true;
+      #audio.enable = true;
+    };
+    deviceTree.enable = true;
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      settings.PasswordAuthentication = false;
+    };
+
+    xserver = {
+      enable = true;
+      desktopManager.kodi = {
+        enable = true;
+        package = pkgs.kodi.withPackages (p:
+          with p; [
+            jellyfin
+            netflix
+            youtube
+            arteplussept
+            sponsorblock
+            inputstreamhelper
+          ]);
+      };
+      displayManager = {
+        autoLogin = {
+          enable = true;
+          user = "kodi";
+        };
+        lightdm = {
+          enable = true;
+          autoLogin.timeout = 3;
+        };
+      };
+    };
+  };
 
   system.stateVersion = "23.11";
 }
