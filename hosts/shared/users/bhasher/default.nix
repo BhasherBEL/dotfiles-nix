f6be4f4a5a974bd8d989fa2f069372516f2aaf92
Brieuc Dubois <git@bhasher.com>
2024-04-07 04:34:37 +0200

    desktop /mnt

--- hosts/shared/users/bhasher/default.nix
+++ hosts/shared/users/bhasher/default.nix
@@ -2,6 +2,9 @@
 let
   ifTheyExist = groups:
     builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+  cifsOptions = [
+    "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,credentials=/etc/nixos/secrets/.smb,uid=1000,gid=100"
+  ];
 in {
   imports = [
     ../../optional/python.nix
@@ -17,6 +20,27 @@ in {
     isNormalUser = true;
     initialPassword = "azerty";
     extraGroups = [ "wheel" "audio" ] ++ ifTheyExist [ "docker" "wireshark" ];
+  };
+
+  fileSystems."/mnt/brieuc" = {
+    device = "//192.168.1.201/brieuc";
+    fsType = "cifs";
+    options = cifsOptions;
+  };
+  fileSystems."/mnt/commun" = {
+    device = "//192.168.1.201/commun";
+    fsType = "cifs";
+    options = cifsOptions;
+  };
+  fileSystems."/mnt/photos" = {
+    device = "//192.168.1.201/photos";
+    fsType = "cifs";
+    options = cifsOptions;
+  };
+  fileSystems."/mnt/movies" = {
+    device = "//192.168.1.201/movies";
+    fsType = "cifs";
+    options = cifsOptions;
   };
 
   environment.systemPackages = with pkgs; [
