81b79d61a66f57b2042e7ff8fbdb5e562b2ab4a6
Brieuc Dubois <git@bhasher.com>
2024-05-08 13:38:16 +0200

    git config

--- home/bhasher.nix
+++ home/bhasher.nix
@@ -16,8 +16,41 @@
   programs = {
     git = {
       enable = true;
-      userName = "Brieuc Dubois";
-      userEmail = "git@bhasher.com";
+      diff-so-fancy.enable = true;
+      extraConfig = {
+        user = {
+          name = "Brieuc Dubois";
+          email = "git@bhasher.com";
+        };
+        # TODO
+        commit.gpgSign = false;
+        init.defaultBranch = "master";
+        gpg.program = "gpg";
+        color = {
+          ui = "auto";
+          diff = "auto";
+          status = "auto";
+          branch = "auto";
+        };
+        help.autocorrect = 30;
+        alias = {
+          graph = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold blue)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+          s = "status";
+          d = "diff";
+          c = "commit";
+        };
+        push.default = "simple";
+      };
+    };
+    ssh = {
+      enable = true;
+      #matchBlocks = {
+      #	"shp" = {
+      #		host = "192.168.1.221"
+      #		user = "shp";
+      #
+      #	};
+      #};
     };
     firefox = {
       enable = true;
