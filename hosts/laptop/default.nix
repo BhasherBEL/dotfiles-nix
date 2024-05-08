db3a663d224e6c5610b1ca4511a9bf10f424b8d4
Brieuc Dubois <git@bhasher.com>
2024-05-08 14:16:43 +0200

    Power management

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -11,6 +11,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.luks.devices."luks-bf6108ec-6c51-44c7-b5cd-88101ee16aa0".device = "/dev/disk/by-uuid/bf6108ec-6c51-44c7-b5cd-88101ee16aa0";
 
+  powerManagement.enable = true;
+
   networking = {
     hostName = "laptop";
 
@@ -31,10 +33,29 @@
     };
   };
 
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "bhasher";
+  services = {
+    displayManager.autoLogin = {
+      enable = true;
+      user = "bhasher";
+    };
+    # Prevent overheating of Intel CPUs
+    thermald.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+      };
+    };
   };
 
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";
 }
