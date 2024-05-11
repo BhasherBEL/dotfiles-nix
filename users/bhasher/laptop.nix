ddbde542fe1e788b05b245e2e59872f366f8b3ee
Brieuc Dubois <git@bhasher.com>
2024-05-11 19:33:28 +0200

    Laptop wg config

--- users/bhasher/laptop.nix
+++ users/bhasher/laptop.nix
@@ -2,15 +2,20 @@
 {
   imports = [ ./default.nix ];
 
+  sops.secrets = {
+    "wg/bxl-shp/laptop/key" = { };
+    "wg/bxl-shp/laptop/psk" = { };
+  };
+
   networking.wg-quick.interfaces.bxl-shp = {
     address = [ "10.15.14.6/32" ];
-    privateKeyFile = "/etc/wireguard/bxl-shp.key";
+    privateKeyFile = "/run/secrets/wg/bxl-shp/laptop/key";
     dns = [ "10.15.14.1" ];
     autostart = true;
     peers = [
       {
         publicKey = "Ft1qUCCs9GkpUfiotZU9Ueq1e9ncXr0PwWEyfLoc6Vs=";
-        presharedKeyFile = "/etc/wireguard/bxl-shp.psk";
+        presharedKeyFile = "/run/secrets/wg/bxl-shp/laptop/psk";
         allowedIPs = [ "0.0.0.0/0" ];
         endpoint = "vpn.bhasher.com:51822";
         persistentKeepalive = 25;
