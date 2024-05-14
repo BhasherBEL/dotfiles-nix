ed3bc53d117c589f272731834212d128b3298d9a
Brieuc Dubois <git@bhasher.com>
2024-05-14 18:03:01 +0200

    remove mistral API key

--- home/shared/global/apps/nvim/config/nvim/lua/plugins/mistral.lua
+++ home/shared/global/apps/nvim/config/nvim/lua/plugins/mistral.lua
@@ -4,7 +4,7 @@ return {
 		--event = "VeryLazy",
 		config = function()
 			require("chatgpt").setup({
-				api_key_cmd = "echo 'G5aUAKGcxklpCShv0b1c1zoJjiYgYPj2'",
+				api_key_cmd = "echo ''",
 			})
 		end,
 		dependencies = {
