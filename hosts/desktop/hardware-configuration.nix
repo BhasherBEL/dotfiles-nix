5ec2a6024e5ce2a33892ee99c264b28b015f6094
Brieuc Dubois <git@bhasher.com>
2024-05-31 22:27:49 +0200

    Disable swap on desktop

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -33,13 +33,6 @@
     fsType = "vfat";
   };
 
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 32 * 1024;
-    }
-  ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
