dca2d5288e2b778384613250d26195040cdf5f66
Brieuc Dubois <git@bhasher.com>
2024-06-02 20:04:08 +0200

    Tmp fix for aw-awatcher

--- home/shared/pc/apps/desktop/activitywatch.nix
+++ home/shared/pc/apps/desktop/activitywatch.nix
@@ -1,25 +1,20 @@
 { pkgs, ... }:
+let
+
+  nur-bhasher = import (builtins.fetchTarball {
+    url = "https://github.com/bhasherbel/nur-packages/archive/1daabf2e5fe5f01cde1e4d69751aa2a048af25d3.tar.gz";
+    sha256 = "1pw0k5yxqlx8q6lnd113y4lx3qqqysxhp025s7xzsssp9na2gfh3";
+  }) { inherit pkgs; };
+in
 {
+
   services.activitywatch = {
     enable = true;
     package = pkgs.aw-server-rust;
     watchers = {
-      aw-watcher-afk = {
-        package = pkgs.activitywatch;
-        settings = {
-          timeout = 300;
-          poll_time = 2;
-        };
-      };
-      aw-watcher-window-wayland = {
-        package = pkgs.aw-watcher-window-wayland;
-        settings = {
-          poll_time = 1;
-          exclude_title = true;
-        };
-      };
-      aw-watcher-vim = {
-        package = pkgs.activitywatch;
+      aw-awatcher = {
+        package = nur-bhasher.aw-awatcher;
+        executable = "awatcher";
       };
     };
   };
