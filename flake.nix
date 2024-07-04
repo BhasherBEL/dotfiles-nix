b4fbd5d62b98c77e8c3765613aa7d654924238f1
Brieuc Dubois <git@bhasher.com>
2024-07-04 22:06:52 +0200

    Simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -3,74 +3,54 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    hyprsome.url = "github:sopa0/hyprsome";
+    nur.url = "github:nix-community/NUR";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    sops-nix.url = "github:Mic92/sops-nix";
+    firefox.url = "github:nix-community/flake-firefox-nightly";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # By-screen workspaces
-    hyprsome.url = "github:sopa0/hyprsome";
-
-    nur.url = "github:nix-community/NUR";
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Secret management
-    sops-nix.url = "github:Mic92/sops-nix";
-
-    firefox.url = "github:nix-community/flake-firefox-nightly";
   };
 
   outputs =
     { nixpkgs, home-manager, ... }@inputs:
     let
       lib = nixpkgs.lib // home-manager.lib;
+
+      makeNixosSystem =
+        name: extraModules:
+        nixpkgs.lib.nixosSystem rec {
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            { nixpkgs.overlays = [ inputs.nur.overlay ]; }
+            home-manager.nixosModules.default
+            { home-manager.extraSpecialArgs = specialArgs; }
+          ] ++ extraModules;
+        };
     in
     {
       inherit lib;
 
       nixosConfigurations = {
-        desktop = nixpkgs.lib.nixosSystem rec {
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            { nixpkgs.overlays = [ inputs.nur.overlay ]; }
-            ./hosts/desktop
-            ./users/bhasher/desktop.nix
-            home-manager.nixosModules.default
-            { home-manager.extraSpecialArgs = specialArgs; }
-          ];
-        };
-        laptop = nixpkgs.lib.nixosSystem rec {
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            { nixpkgs.overlays = [ inputs.nur.overlay ]; }
-            ./hosts/laptop
-            ./users/bhasher/laptop.nix
-            home-manager.nixosModules.default
-            { home-manager.extraSpecialArgs = specialArgs; }
-          ];
-        };
-        media-center = nixpkgs.lib.nixosSystem {
-          modules = [
-            { nixpkgs.overlays = [ inputs.nur.overlay ]; }
-            ./hosts/media-center
-            ./users/kodi
-            inputs.nixos-hardware.nixosModules.raspberry-pi-4
-          ];
-          specialArgs = {
-            inherit inputs;
-          };
-        };
+        desktop = makeNixosSystem "desktop" [
+          ./hosts/desktop
+          ./users/bhasher/desktop.nix
+        ];
+
+        laptop = makeNixosSystem "laptop" [
+          ./hosts/laptop
+          ./users/bhasher/laptop.nix
+        ];
+
+        media-center = makeNixosSystem "media-center" [
+          ./hosts/media-center
+          ./users/kodi
+          inputs.nixos-hardware.nixosModules.raspberry-pi-4
+        ];
       };
     };
 }
