a0e2129d95a4b32b5408870d2f61221d57980ac9
Brieuc Dubois <git@bhasher.com>
2024-07-19 15:05:33 +0200

    Hyprlock, the nix way

--- home/shared/pc/hyprland/default.nix
+++ home/shared/pc/hyprland/default.nix
@@ -272,7 +272,94 @@
         sort = true;
       };
     };
-    hyprlock.enable = true;
+    hyprlock = {
+      enable = true;
+      settings = {
+        source = "$HOME/.config/hypr/themes/mocha.conf";
+
+        "$accent" = "$mauve";
+        "$accentAlpha" = "$mauveAlpha";
+        "$font" = "JetBrainsMono Nerd Font";
+
+        # GENERAL
+        general = {
+          disable_loading_bar = true;
+          hide_cursor = true;
+          grace = 15;
+        };
+
+        # BACKGROUND
+        background = [
+          {
+            #path = ~/.config/background
+            blur_passes = 0;
+            color = "rgba(25, 20, 30, 1.0)";
+          }
+        ];
+
+        # TIME
+        label = [
+          {
+            text = "cmd[update:30000] echo \"$(date +\"%R\")\"";
+            color = "$text";
+            font_size = 90;
+            font_family = "$font";
+            position = "-30, 0";
+            halign = "right";
+            valign = "top";
+          }
+
+          # DATE 
+          {
+            text = "cmd[update:43200000] echo \"$(date +\"%A, %d %B %Y\")\"";
+            color = "$text";
+            font_size = 25;
+            font_family = "$font";
+            position = "-30, -150";
+            halign = "right";
+            valign = "top";
+          }
+        ];
+
+        # USER AVATAR
+
+        image = [
+          {
+            path = "~/.face.png";
+            size = 100;
+            border_color = "$accent";
+
+            position = "0, 75";
+            halign = "center";
+            valign = "center";
+          }
+        ];
+
+        # INPUT FIELD
+        input-field = [
+          {
+            size = "300, 60";
+            outline_thickness = 4;
+            dots_size = 0.2;
+            dots_spacing = 0.2;
+            dots_center = true;
+            outer_color = "$accent";
+            inner_color = "$surface0";
+            font_color = "$text";
+            fade_on_empty = false;
+            placeholder_text = "<span foreground=\"##$textAlpha\"> <i>Logged in as</i> <span foreground=\"##$accentAlpha\">$USER</span></span>";
+            hide_input = false;
+            check_color = "$accent";
+            fail_color = "$red";
+            fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+            capslock_color = "$yellow";
+            position = "0, -35";
+            halign = "center";
+            valign = "center";
+          }
+        ];
+      };
+    };
   };
 
   services = {
@@ -338,9 +425,6 @@
       source = ./config/hypr/scripts;
       recursive = true;
       executable = true;
-    };
-    "${config.xdg.configHome}/hypr/hyprlock.conf" = {
-      source = ./config/hypr/hyprlock.conf;
     };
     "${config.xdg.configHome}/swaync" = {
       source = ./config/swaync;
