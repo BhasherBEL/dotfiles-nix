6efb02700def811733436c6fadc1d71eae957eeb
Brieuc Dubois <git@bhasher.com>
2024-08-15 21:39:58 +0200

    tlp -> auto-cpufreq

--- hosts/shared/laptop/default.nix
+++ hosts/shared/laptop/default.nix
@@ -5,19 +5,19 @@
   services = {
     # Prevent overheating of Intel CPUs
     thermald.enable = true;
-    tlp = {
+    auto-cpufreq = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
-
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "normal";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 50;
+        battery = {
+          governor = "schedutil";
+          turbo = "never";
+          energy_performance_preference = "balance_power";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+          energy_performance_preference = "performance";
+        };
       };
     };
   };
