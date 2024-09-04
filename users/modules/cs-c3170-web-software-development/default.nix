bcb14983ef74d1e51927b466148e5dedcc757461
Brieuc Dubois <git@bhasher.com>
2024-09-04 21:05:36 +0300

    cs-c3170 class

---
+++ users/modules/cs-c3170-web-software-development/default.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  cs-c3170-web-software-developmentcfg = config.modules.cs-c3170-web-software-development;
+in
+{
+  options = {
+    modules.cs-c3170-web-software-development.enable = lib.mkEnableOption "Enable cs-c3170-web-software-development";
+  };
+
+  config = lib.mkIf cs-c3170-web-software-developmentcfg.enable {
+    environment.systemPackages = [ pkgs.deno ];
+  };
+}
