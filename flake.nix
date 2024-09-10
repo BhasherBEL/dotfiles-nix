24e35189f2dcb57ce4bd1428186c3a2097d97faa
Brieuc Dubois <git@bhasher.com>
2024-09-10 15:37:17 +0300

    Nixvim follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     };
     nixvim = {
       url = "github:nix-community/nixvim";
-      #inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-programs-sqlite = {
       url = "github:wamserma/flake-programs-sqlite";
