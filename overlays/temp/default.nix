598cbab15fedb754379dc4f4d1c83202d0dd0453
Brieuc Dubois <git@bhasher.com>
2024-10-31 14:33:02 +0200

    Update

--- overlays/temp/default.nix
+++ overlays/temp/default.nix
@@ -1,3 +1,8 @@
-self: super:
-{
+self: super: {
+  # https://github.com/NixOS/nixpkgs/issues/349012
+  openvpn3 = super.openvpn3.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      ./openvpn3.patch
+    ];
+  });
 }
