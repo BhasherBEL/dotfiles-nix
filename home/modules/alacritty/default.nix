8d0013365e868de7938638c39c7acb58518ae669
Brieuc Dubois <git@bhasher.com>
2025-03-15 13:43:25 +0100

    Alacritty fonts

--- home/modules/alacritty/default.nix
+++ home/modules/alacritty/default.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   alacrittycfg = config.modules.alacritty;
 in
@@ -8,12 +13,33 @@ in
   };
 
   config = lib.mkIf alacrittycfg.enable {
+
+    fonts.fontconfig.enable = true;
+
+    home.packages = [
+      pkgs.nerd-fonts.fira-code
+    ];
+
     programs.alacritty = {
       enable = true;
       settings = {
         window.padding = {
           x = 2;
           y = 2;
+        };
+        font = {
+          normal = {
+            family = lib.mkForce "Fira Code Nerd Font";
+            style = "Regular";
+          };
+          bold = {
+            family = lib.mkForce "Fira Code Nerd Font";
+            style = "Bold";
+          };
+          italic = {
+            family = lib.mkForce "Fira Code Nerd Font";
+            style = "Italic";
+          };
         };
       };
     };
