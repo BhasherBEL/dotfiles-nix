7501bea6978cd663feef419806c34e68ead3e5cf
Brieuc Dubois <git@bhasher.com>
2025-06-08 17:41:05 +0200

    Fix bluetooth

--- users/modules/bluetooth/default.nix
+++ users/modules/bluetooth/default.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   bluetoothcfg = config.modules.bluetooth;
 in
@@ -11,6 +16,29 @@ in
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
+      package = pkgs.bluez;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+        };
+      };
+    };
+
+    services.blueman.enable = true;
+
+    systemd.services.bluetooth-power-on = {
+      description = "Power on Bluetooth adapter and unblock rfkill";
+      after = [ "bluetooth.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = [
+          "${pkgs.util-linux}/bin/rfkill unblock bluetooth"
+          "${pkgs.bluez}/bin/bluetoothctl power on"
+        ];
+      };
     };
   };
 }
