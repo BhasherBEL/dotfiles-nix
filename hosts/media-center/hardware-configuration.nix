45a77f0db6b47cb659a2e429ff623265f1b7eb21
Brieuc Dubois <git@bhasher.com>
2025-06-16 15:29:55 +0200

    Fix fileSystems for media-center

--- hosts/media-center/hardware-configuration.nix
+++ hosts/media-center/hardware-configuration.nix
@@ -10,23 +10,43 @@
     ];
   };
 
-  fileSystems."/" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = [
-      "defaults"
-      "size=25%"
-      "mode=755"
-    ];
-  };
-  fileSystems."/home/kodi" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = [
-      "defaults"
-      "size=25%"
-      "mode=777"
-    ];
+  fileSystems = {
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = [
+        "defaults"
+        "size=25%"
+        "mode=755"
+      ];
+    };
+    "/home/kodi" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = [
+        "defaults"
+        "size=25%"
+        "mode=777"
+      ];
+    };
+    "sd-card" = {
+      neededForBoot = true;
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
+    "nix" = {
+      device = "/sd-card/nix";
+      options = [ "bind" ];
+    };
+    "boot" = {
+      device = "/sd-card/boot";
+      options = [ "bind" ];
+    };
+    "/persistent" = {
+      neededForBoot = true;
+      device = "/sd-card/persistent";
+      options = [ "bind" ];
+    };
   };
 
   networking.useDHCP = lib.mkDefault true;
