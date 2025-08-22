c239d0738e9bae103bf778e882ba848cc0e2cded
Brieuc Dubois <git@bhasher.com>
2025-08-22 19:28:03 +0200

    Update & vpn update

--- users/bhasher/desktop.nix
+++ users/bhasher/desktop.nix
@@ -12,18 +12,20 @@
   networking.wg-quick.interfaces.bxl-shp = {
     address = [ "10.15.14.3/32" ];
     privateKeyFile = "/run/secrets/wg/bxl-shp/desktop/key";
-    dns = [
-      "10.15.14.1"
-      "1.1.1.1"
-    ];
+    # dns = [
+    #   # "10.15.14.1"
+    #   "1.1.1.1"
+    # ];
     autostart = true;
     peers = [
       {
         publicKey = "Ft1qUCCs9GkpUfiotZU9Ueq1e9ncXr0PwWEyfLoc6Vs=";
         presharedKeyFile = "/run/secrets/wg/bxl-shp/desktop/psk";
         allowedIPs = [
-          "0.0.0.0/0"
+          # "0.0.0.0/0"
+          "10.15.14.0/24"
           "192.168.1.0/24"
+          # "91.182.226.236/32"
         ];
         endpoint = "vpn.bhasher.com:51822";
         persistentKeepalive = 25;
