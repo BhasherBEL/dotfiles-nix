06af8af58a469fedbdf1f3c20055c3631701bd28
Brieuc Dubois <git@bhasher.com>
2025-09-02 20:33:04 +0200

    Fixed IP

--- hosts/media-center/default.nix
+++ hosts/media-center/default.nix
@@ -90,8 +90,22 @@
     raspberrypi-eeprom
   ];
 
-  networking.hostName = "media-center";
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "media-center";
+    networkmanager.enable = true;
+    dhcpcd.enable = false;
+    defaultGateway = "192.168.0.1";
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    interfaces.end0.ipv4.addresses = [
+      {
+        address = "192.168.0.200";
+        prefixLength = 24;
+      }
+    ];
+  };
 
   services = {
     openssh.enable = true;
