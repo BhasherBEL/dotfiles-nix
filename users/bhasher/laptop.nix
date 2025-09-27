88384310819e96099b1532a88f9deacad282185e
Brieuc Dubois <git@bhasher.com>
2025-09-27 12:01:30 +0200

    Update laptop vpn

--- users/bhasher/laptop.nix
+++ users/bhasher/laptop.nix
@@ -1,37 +1,24 @@
-{ lib, ... }:
+{ ... }:
 {
   imports = [
     ./default.nix
   ];
 
-  sops.secrets = {
-    "wg/bxl-shp/laptop/key" = { };
-    "wg/bxl-shp/laptop/psk" = { };
-  };
-
-  networking = {
-    wg-quick.interfaces.bxl-shp = {
-      address = [ "10.20.0.3/32" ];
-      privateKeyFile = "/run/secrets/wg/bxl-shp/laptop/key";
-      dns = [ "10.20.0.1" ];
-      autostart = true;
-      peers = [
-        {
-          publicKey = "Ft1qUCCs9GkpUfiotZU9Ueq1e9ncXr0PwWEyfLoc6Vs=";
-          allowedIPs = [
-            "10.20.0.0/24"
-            "192.168.1.0/24"
-          ];
-          endpoint = "vpn.wol.bhasher.com:51824";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
-
   modules = {
     bluetooth.enable = true;
     docker.enable = true;
-    ipv6.disable = true;
+  };
+
+  hostServices.vpn-client = {
+    enable = true;
+    ipv4 = "10.20.0.3/24";
+    # ipv6 = "fd8c:70ee:bdd8:0:1::3/128";
+    privateKeySecret = "wg/bxl-shp/laptop/key";
+    route = {
+      all = true;
+      bxl = true;
+      wol = true;
+    };
+    autostart = true;
   };
 }
