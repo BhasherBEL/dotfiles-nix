59ad79282b5dfc8bc732bb428f37e821cf1f83b0
Brieuc Dubois <git@bhasher.com>
2025-12-03 15:20:54 +0100

    Fix VPN & dyndns

--- services/dyndns.nix
+++ services/dyndns.nix
@@ -7,8 +7,8 @@
     };
   };
 
-  config =
-    lib.mkIf config.hostServices.dyndns.wol.enable {
+  config = lib.mkMerge [
+    (lib.mkIf config.hostServices.dyndns.wol.enable {
       sops.secrets = {
         "services/dyndns/wol" = { };
       };
@@ -17,16 +17,19 @@
         enable = true;
         username = "bhasher.com-shp";
         server = "www.ovh.com";
-        protocol = "dyndns2";
+        protocol = "ovh";
         passwordFile = "/run/secrets/services/dyndns/wol";
         interval = "10min";
         usev4 = "webv4, webv4=ipify-ipv4";
+        usev6 = "disabled";
         domains = [
           "wol.bhasher.com"
         ];
       };
     }
-    // lib.mkIf config.hostServices.dyndns.bxl.enable {
+
+    )
+    (lib.mkIf config.hostServices.dyndns.bxl.enable {
       sops.secrets = {
         "services/dyndns/bxl" = { };
       };
@@ -35,16 +38,15 @@
         enable = true;
         username = "bhasher.com-bxl";
         server = "www.ovh.com";
-        # protocol = "dyndns2";
         protocol = "ovh";
         passwordFile = "/run/secrets/services/dyndns/bxl";
         interval = "10min";
-        # use = "webv4, webv4=ipify-ipv4";
         usev4 = "webv4,webv4=ipify-ipv4";
         usev6 = "disabled";
         domains = [
           "bxl.bhasher.com"
         ];
       };
-    };
+    })
+  ];
 }
