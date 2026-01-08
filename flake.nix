315af03d3fa88d5b12df8dc9411d0271196d90b7
Brieuc Dubois <git@bhasher.com>
2026-01-08 17:53:01 +0100

    Update & fix stylix

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     simple-nixos-mailserver = {
